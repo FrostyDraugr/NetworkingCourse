ac9b2de586baf909bd8d43539955d030a16a41d3
JensSundqvist <jens.sundqvist@edu.futuregames.nu>
2024-03-12 09:45:27 +0100

    Death, Respawn + Token Respawn fixed
    Added proper death with respawn tokens by having the owner listening for death updates. When the player dies they can trigger a respawn reqeust which is handled by the server.

--- Assets/Scripts/Player/Health.cs
+++ Assets/Scripts/Player/Health.cs
@@ -5,6 +5,7 @@ using Unity.Netcode;
 using Unity.Services.Lobbies.Models;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class Health : NetworkBehaviour
 {
@@ -14,27 +15,57 @@ public class Health : NetworkBehaviour
     [SerializeField] GameObject _deathScreen;
     private NetworkVariable<bool> _dead = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
     private NetworkVariable<int> _respawnTokens = new NetworkVariable<int>(3, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+    [SerializeField] Text _tokensLeftText;
     private NetworkVariable<bool> _respawnRequest = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
 
     public override void OnNetworkSpawn()
     {
         _dead.OnValueChanged += Death;
 
+        if (IsOwner)
+        {
+            _respawnTokens.OnValueChanged += UpdateDeathText;
+        }
+
         if (!IsServer) return;
         _currentHealth.Value = 100;
+        _respawnRequest.OnValueChanged += RespawnHandler;
+    }
+
+    private void UpdateDeathText(int previousValue, int newValue)
+    {
+        if (!IsOwner) return;
+
+        _tokensLeftText.text = "Tokens Left: " + _respawnTokens.Value;
     }
 
     private void Death(bool previousValue, bool newValue)
     {
-        if (newValue == false) return;
-
-        if (IsOwner)
+        if (newValue == true)
         {
-            transform.position = new Vector3(1000, 1000, 0);
+            if (IsOwner)
+            {
+                transform.position = new Vector3(1000, 1000, 0);
 
-            _playerController.enabled = false;
-            _deathScreen.SetActive(true);
+                _playerController.enabled = false;
+                _deathScreen.SetActive(true);
+            }
         }
+        else
+        {
+            if (IsOwner)
+            {
+                int xPosition = Random.Range(-4, 4);
+                int yPosition = Random.Range(-2, 2);
+                transform.position = new Vector3(xPosition, yPosition, 0);
+
+                _playerController.enabled = true;
+                _deathScreen.SetActive(false);
+                _respawnRequest.Value = false;
+            }
+        }
+
+
     }
 
     public void TakeDamage(int damage)
@@ -50,21 +81,26 @@ public class Health : NetworkBehaviour
         }
     }
 
-    IEnumerator RespawnEvent()
+    public void RespawnRequest()
     {
-        yield return new WaitForSeconds(2);
+        if (!IsOwner) return;
 
-        int xPosition = Random.Range(-4, 4);
-        int yPosition = Random.Range(-2, 2);
-        transform.position = new Vector3(xPosition, yPosition, 0);
+        _respawnRequest.Value = true;
+    }
 
-        _playerController.enabled = true;
-        _deathScreen.SetActive(false);
+    private void RespawnHandler(bool previousValue, bool newValue)
+    {
+        if (!IsServer || newValue == false) return;
 
-        _firingController.AddAmmo(100);
-        _currentHealth.Value = 100;
+        if (_respawnTokens.Value > 0)
+        {
+            _respawnTokens.Value--;
+            Debug.Log(_respawnTokens.Value);
+            _firingController.AddAmmo(100);
+            _currentHealth.Value = 100;
+            _dead.Value = false;
+        }
 
-        yield break;
     }
 
     public void Heal(int healing)
